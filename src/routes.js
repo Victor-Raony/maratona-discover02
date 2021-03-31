7298604992be9fca6174ebb1ea30318e55934161
Victor Raony <victor_raony12@hotmail.com>
2021-03-31 13:35:11 -0400

    firts commit

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,66 @@ const routes = express.Router()
 
 const views = __dirname + '/views/'
 
-routes.get('/', (req, res) => res.render(views + "index"))
+const profile = {
+    name: "Victor Raony",
+    avatar: "https://github.com/victor-raony.png",
+    "monthly-budget": 3000,
+    "days-per-week": 5,
+    "hours-per-day": 5,
+    "vacation-per-year": 4
+}
+
+const jobs = [
+    {
+        id: 1,
+        name: "Pizzaria Guloso",
+        "daily-hours": 2,
+        "total-hours": 60,
+        created_at: Date.now()
+    },
+    {
+        id: 2,
+        name: "OneTwo Project",
+        "daily-hours": 3,
+        "total-hours": 47,
+        created_at: Date.now()
+    }
+]
+
+routes.get('/', (req, res) => {
+
+    const updateJobs = jobs.map((job) => {
+
+        const remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed()
+
+        const createdDate = new Date(job.created_at)
+        const dueDay = createdDate.getDate() + Number(remainingDays)
+        const dueDate = createdDate.setDate(dueDate)
+
+        const timeDiffinMs = dueDate - Date.now()
+
+        return job
+    })
+
+   return res.render(views + "index", { jobs })
+})
+
 routes.get('/job', (req, res) => res.render(views + "job"))
 routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
-routes.get('/profile', (req, res) => res.render(views + "profile"))
+routes.post('/job', (req, res) => {
+
+    const lastId = jobs[jobs.length - 1]?.id || 1;
+
+    jobs.push({
+        id: lastId + 1,
+        name: req.body.name,
+        "daily-hours": req.body["daily-hours"],
+        "total-hours": req.body["total-hours"],
+        created_at: Date.now()
+    })
+
+    return res.redirect('/')
+})
+routes.get('/profile', (req, res) => res.render(views + "profile", {profile}))
 
  module.exports = routes;
